1b270d5d69bffa2c1662795fb30b18611ffbb8a1
DrC <dclinkenbeard@csumb.edu>
2023-10-01 14:37:01 -0700

    Finished adding the unit tests.  Neat.

--- code/index.ts
+++ code/index.ts
@@ -1,4 +1,4 @@
-function add (numbers: string): number {
+export function add (numbers: string): number {
     let integers = numbers.split(',').map(x => parseInt(x));
     let negatives = integers.filter(x => x < 0);
     
@@ -9,6 +9,6 @@ function add (numbers: string): number {
 
     return integers.filter(x => x <= 1000).reduce((a,b) => a+b ,0);
 }
-
-let result = add('1,2,4,5');
-console.log(result);
+// Removing this since i have a test file now.
+// let result = add('1,2,4,5');
+// console.log(result);
--- test/index.test.ts
+++ test/index.test.ts
@@ -3,5 +3,10 @@ import { add } from '../code/index';
 describe('testing index file', () => {
     test('empty string should result in zero', () =>{
         expect(add('')).toBe(0);
+        expect(add('1')).toBe(1);
+        expect(add('1001')).toBe(0);
+        expect(add('1,2,3')).toBe(6);
+        expect(add('1,2,1000')).toBe(1003);
+        // expect(add('-10')).toThrowError();
     });
 });
