14940fd979f710f1e2790231002274a846ae8949
daclink <drew@daclink.com>
2023-10-01 15:54:38 -0700

    added comments and additional unit tests.

--- code/index.ts
+++ code/index.ts
@@ -1,14 +1,22 @@
+/**
+ * splits a comma seperated string and converts the tokens to integers. 
+ * Returns the sum of the integers
+ * 
+ * @throws RangeError if there is a negative number
+ * @param numbers a comma seperated string 0 – 1000 inclusive
+ * @returns the sum of all integers between 0 and 1000 inclusive
+ */
 export function add (numbers: string): number {
-    let integers = numbers.split(',').map(x => parseInt(x));
+    let integers = numbers.split(',')
+    .map(x => parseInt(x))
+    .filter(x => !isNaN(x));
+    
     let negatives = integers.filter(x => x < 0);
     
     if (negatives.length > 0){
-        // throw new RangeError('Negatives are not allowed ' + negatives.join(', '));
-        throw new RangeError(`Negatives are not allowed  ${negatives.join(', ')}`);
+        // throw new RangeError(`Negatives are not allowed  ${negatives.join(', ')}`);
+        throw new RangeError('Negatives are not allowed' + negatives.join(', '));
     }
 
     return integers.filter(x => x <= 1000).reduce((a,b) => a+b ,0);
 }
-// Removing this since i have a test file now.
-// let result = add('1,2,4,5');
-// console.log(result);
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,18 @@
 import { add } from '../code/index';
 
 describe('testing index file', () => {
-    test('empty string should result in zero', () =>{
+    test('Empty string should result in zero', () =>{
         expect(add('')).toBe(0);
+    });
+    test('Functionality test', () =>{
         expect(add('1')).toBe(1);
         expect(add('1001')).toBe(0);
         expect(add('1,2,3')).toBe(6);
         expect(add('1,2,1000')).toBe(1003);
-        // expect(add('-10')).toThrowError();
+        expect(add('1,2,9999')).toBe(3);
+    });
+    test('Negatives should throw range error', () =>{
+        expect(() => add('-10')).toThrow(RangeError);
+        expect(() => add('1,2,-10')).toThrow(RangeError);
     });
 });
